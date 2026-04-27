dc77c93c21d329a3674bf45e3e1522777ddcdfc7
pistachio_ice <feiwu2442@outlook.com>
2026-04-27 14:01:30 +0800

    Enemy: DamageManager; EnemyShoot
    added scripts for enemies, destroy on damage, enemy shoot

---
+++ Assets/Scripts/Characters/Enemy/DamageManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    [SerializeField] private float damage = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
